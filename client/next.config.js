67091ea5ddce26f41b346a35a25222c475cbe687
Ken Nzau <kenn.nzau@gmail.com>
2024-04-15 08:52:09 -0300

    add ui libraries

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		dangerouslyAllowSVG: true,
+		contentDispositionType: "attachment",
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com"
+			}
+		]
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
