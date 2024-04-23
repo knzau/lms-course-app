19ef7fe1f79bc7a9fd70a3107032e465d0049728
Ken Nzau <kenn.nzau@gmail.com>
2024-04-22 21:27:35 -0300

    Refactor: make navbar fixed

--- client/src/components/modules/NavBar.tsx
+++ client/src/components/modules/NavBar.tsx
@@ -8,28 +8,30 @@ import { Button } from "../ui/Button";
 
 const NavBar = () => {
 	return (
-		<>
+		<div className="w-full h-[112px] fixed z-50">
 			<div className="flex w-full bg-secondary justify-center text-input text-sm h-8 items-center">
 				Free Courses 🌟 Sale Ends Soon, Get It Now &rarr;
 			</div>
-			<MaxWidthWrapper className="flex gap-4 h-[80px] items-center justify-between bg-white">
-				<Image src="/SkillBridgeLogo.svg" width={238} height={44} alt="logo" />
+			<div className="w-full bg-white">
+				<MaxWidthWrapper className="flex gap-4 h-[80px] items-center justify-between  w-full">
+					<Image src="/SkillBridgeLogo.svg" width={238} height={44} alt="logo" />
 
-				<ul className="hidden md:flex gap-8">
-					{Object.entries(navMenuItems).map(([link, title]) => {
-						return (
-							<Link key={link} href={link}>
-								{title}
-							</Link>
-						);
-					})}
-				</ul>
-				<div className="flex gap-8">
-					<Button>Register for Free</Button>
-					<Button variant="outline">Login</Button>
-				</div>
-			</MaxWidthWrapper>
-		</>
+					<ul className="hidden md:flex gap-8">
+						{Object.entries(navMenuItems).map(([link, title]) => {
+							return (
+								<Link key={link} href={link}>
+									{title}
+								</Link>
+							);
+						})}
+					</ul>
+					<div className="flex gap-8">
+						<Button>Register for Free</Button>
+						<Button variant="outline">Login</Button>
+					</div>
+				</MaxWidthWrapper>
+			</div>
+		</div>
 	);
 };
 
